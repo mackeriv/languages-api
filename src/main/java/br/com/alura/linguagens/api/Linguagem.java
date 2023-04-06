4aada563cd612b992122c3d503da3002655627a4
mackeriv <mackeriv@gmail.com>
2023-04-06 01:47:54 -0300

    Converted Linguagem class into "record".

--- src/main/java/br/com/alura/linguagens/api/Linguagem.java
+++ src/main/java/br/com/alura/linguagens/api/Linguagem.java
@@ -4,34 +4,4 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "principaisLinguagens")
-public class Linguagem {
-
-    @Id
-    private final String id;
-    private final String title;
-    private final String image;
-    private final int ranking;
-
-    public Linguagem(String id, String title, String image, int ranking) {
-        this.id = id;
-        this.title = title;
-        this.image = image;
-        this.ranking = ranking;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public int getRanking() {
-        return ranking;
-    }
-}
+public record Linguagem(@Id String id, String title, String image, int ranking) {}
